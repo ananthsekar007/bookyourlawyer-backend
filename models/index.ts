5a3c18d4f574eaf075fc8bdb952e4a6c2d0e9b80
Ananth <ananth.sekar@gogosoon.com>
2021-05-16 22:09:04 +0530

    [Ananth] Adds common import page

---
+++ models/index.ts
@@ -0,0 +1,13 @@
+import Clients from "./Clients";
+import Lawyers from "./Lawyers";
+import Profile from "./Profile";
+import User from "./User";
+import Users_Token from "./Users_Tokens";
+
+export {
+    Clients,
+    Lawyers,
+    Profile,
+    User,
+    Users_Token
+}
