9bc3af6d0d1cf4030c73b94b7df62090f14ab823
Ananth <ananth.sekar@gogosoon.com>
2021-05-16 22:06:37 +0530

    [Ananth] Removes unwanted code

--- models/Users_Tokens.ts
+++ models/Users_Tokens.ts
@@ -10,21 +10,18 @@ export default class Users_Tokens extends Model {
   public user_id!: number;
   public device_ip!: string;
   public device_useragent!: string;
-  public notification_token!: string;
 
   public static async getNewUserToken(user: User, context: BaseContext, additional_jwt_data?) {
     const token = generateDbToken();
 
     const device_ip = context.req.ip || "";
     const device_useragent = context.req.headers["user-agent"] || "";
-    const notification_token = "";
 
     const userToken: any = await Users_Tokens.create({
       user_token: token,
       user_id: user.id,
       device_ip,
-      device_useragent,
-      notification_token: notification_token,
+      device_useragent
     });
     if (!userToken) throw new Error("Error in token generation. Please contact admin.");
     return generateJWTToken(token, additional_jwt_data);
@@ -46,14 +43,12 @@ Users_Tokens.init(
     },
     user_id: {
       type: DataTypes.NUMBER,
+      references: {model: "users", key: "id"}
     },
     device_ip: {
       type: DataTypes.NUMBER,
     },
     device_useragent: {
-      type: DataTypes.STRING,
-    },
-    notification_token: {
       type: DataTypes.STRING,
     },
   },
