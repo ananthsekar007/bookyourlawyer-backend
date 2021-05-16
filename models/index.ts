41f73f478f5c16037728999c62ca1f9073ca8601
Ananth <ananth.sekar@gogosoon.com>
2021-05-16 22:10:41 +0530

    [Ananth] Changes imports

--- models/index.ts
+++ models/index.ts
@@ -2,12 +2,12 @@ import Clients from "./Clients";
 import Lawyers from "./Lawyers";
 import Profile from "./Profile";
 import User from "./User";
-import Users_Token from "./Users_Tokens";
+import Users_Tokens from "./Users_Tokens";
 
 export {
     Clients,
     Lawyers,
     Profile,
     User,
-    Users_Token
+    Users_Tokens
 }
